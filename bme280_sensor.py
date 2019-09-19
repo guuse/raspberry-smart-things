4a05f7e3f68acea8d71a5bbf6f1b18549dd5b24d
guus <ekkelenkampguus@gmail.com>
2019-09-19 14:05:06 +0100

    Write and read data to json file
    This is done so a rest api call can be made easy with the json data file
    Json still gets read and outputted to the terminal

--- bme280_sensor.py
+++ bme280_sensor.py
@@ -10,21 +10,29 @@ bus = smbus2.SMBus(port)
 
 bme280.load_calibration_params(bus,address)
 
-date_time = datetime.datetime.now().strftime("%I:%M%p")
+_time = datetime.datetime.now().strftime("%I:%M%p")
+date = datetime.datetime.now().strftime("%d/%m/%y")
 
 while True:
     bme280_data = bme280.sample(bus,address)
     hum  = round(bme280_data.humidity,1)
     temp = round(bme280_data.temperature,1)
-    #print("{0}: Humudity: {1}%".format(date_time, hum))
-    #print("{0}: Temperature: {1}°C".format(date_time, temp))
-    data = {}
-    data['measurements'] = []
-    data['measurements'].append({
+    
+    write_data = {}
+    write_data['measurements'] = []
+    write_data['measurements'].append({
+        'Date': date,
         'Temperature': temp,
         'Humidity': hum
     })    
     with open('data.txt', 'w') as outfile:
-        json.dump(data, outfile)
-    time.sleep(10)
+        json.dump(write_data, outfile)
     
+    with open('data.txt') as json_file:
+        read_data = json.load(json_file)
+        for p in read_data['measurements']:
+            print("{0}: Date: {1}".format(_time, p['Date']))
+            print("{0}: Humidity: {1}%".format(_time, p['Temperature']))
+            print("{0}: Temperature: {1}°C".format(_time, p['Humidity']))
+            print("")
+    time.sleep(10)
