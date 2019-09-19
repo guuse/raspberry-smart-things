d2df6ca9bf81aff889b229683fe45a71ed80b0c2
guus <ekkelenkampguus@gmail.com>
2019-09-19 13:46:41 +0100

    Write sensordata out to json

--- bme280_sensor.py
+++ bme280_sensor.py
@@ -2,6 +2,7 @@ import bme280
 import smbus2
 import time;
 import datetime;
+import json
 
 port = 1
 address = 0x76
@@ -15,6 +16,15 @@ while True:
     bme280_data = bme280.sample(bus,address)
     hum  = round(bme280_data.humidity,1)
     temp = round(bme280_data.temperature,1)
-    print("{0}: Humudity: {1}%".format(date_time, hum))
-    print("{0}: Temperature: {1}°C".format(date_time, temp))
+    #print("{0}: Humudity: {1}%".format(date_time, hum))
+    #print("{0}: Temperature: {1}°C".format(date_time, temp))
+    data = {}
+    data['measurements'] = []
+    data['measurements'].append({
+        'Temperature': temp,
+        'Humidity': hum
+    })    
+    with open('data.txt', 'w') as outfile:
+        json.dump(data, outfile)
     time.sleep(10)
+    
