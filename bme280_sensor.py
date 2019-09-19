f79797efb09f76a69ef54b94cb58e9c578029271
guus <ekkelenkampguus@gmail.com>
2019-09-19 13:14:09 +0100

    First temp and humidity reads with BME280

---
+++ bme280_sensor.py
@@ -0,0 +1,16 @@
+import bme280
+import smbus2
+from time import sleep
+
+port = 1
+address = 0x76
+bus = smbus2.SMBus(port)
+
+bme280.load_calibration_params(bus,address)
+
+while True:
+    bme280_data = bme280.sample(bus,address)
+    humidity  = bme280_data.humidity
+    ambient_temperature = bme280_data.temperature
+    print(humidity, ambient_temperature)
+    sleep(1)
